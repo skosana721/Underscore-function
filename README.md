c85182471d33250ecbcc0a067de366b8a978a1df
Masekolg <masekolg@gmail.com>
2020-07-30 10:57:44 +0200

    function that outputs underscore

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Underscore-function
+
+This a function that receives a number as an input and outputs underscore between each two even numbers. 
---
+++ index.js
@@ -0,0 +1,6 @@
+let result= [str[0]];
+for(let x = 1; x<str.length; x++){
+    if((str[x-1]%2 === 0)&&(str[x]%2 === 0){
+        result.push('-',str[x]);
+    });
+}
